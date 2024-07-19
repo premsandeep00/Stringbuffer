229784f7083e6dc46154dc1faccd9e538d621d79
premsandeep00 <sandeepprem62@gmail.com>
2024-07-19 09:52:44 +0530

    Add files via upload
    added prem.java file

---
+++ Prem.java
@@ -0,0 +1,26 @@
+//package stringbuilder;
+//
+//public class Prem {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//		//creat a Stringbuilder object
+//		StringBuilder str=new StringBuilder();
+//		str.append("java");
+//		System.out.println(str);
+//		
+//		//creat a Stringbuilder using
+//		StringBuilder str1=new StringBuilder("prem");
+//		System.out.println("str1="+str1.toString());
+//		
+//		//capacity StringBuilder
+//		StringBuilder str2=new StringBuilder(10);
+//		System.out.println("str2="+str2.capacity());
+//		
+//		//Stringbuilder using
+//		StringBuilder str3=new StringBuilder(str1.toString());
+//		System.out.println(str3.toString());
+//	}
+//	}
+//
